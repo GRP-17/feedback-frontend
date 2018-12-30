46d5821c8603fdad8179ddb6f11fb3eaeff37c03
George <psygh3@nottingham.ac.uk>
2018-12-30 22:58:05 +0000

    Added the form with validation

--- feedback-frontend-app/src/App.js
+++ feedback-frontend-app/src/App.js
@@ -1,26 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import StarRatings from 'react-star-ratings';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        rating: 5,
+        text: '',
+        characters: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeRating = this.changeRating.bind(this);
+  }
+
+  changeRating(newRating, name) {
+      this.setState({
+          rating: newRating
+      });
+  }
+
+  handleChange(event) {
+      var LocalCharacters = event.target.value.length;
+      var LocalText = event.target.value
+
+      if(LocalCharacters >= 5001) {
+          alert("Your feedback can't be more than 5000 characters");
+          return
+      }
+    this.setState({
+        text: LocalText,
+        characters: LocalCharacters + "/5000 characters"
+    });
+  }
+
+  handleSubmit(event) {
+    alert('Rating: ' + this.state.rating + '\nText: ' + this.state.text);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form id='feedback-form' onSubmit={this.handleSubmit}>
+          <div id='rating-container'>
+              <StarRatings
+                rating={this.state.rating}
+                starRatedColor='rgb(230,67,47)'
+                changeRating={this.changeRating}
+                numberOfStars={5}
+                name='rating'
+                starDimension='30px'
+              />
+          </div>
+          <div id='text-container'>
+              <textarea rows='40' cols='50' value={this.state.text} onChange={this.handleChange}/>
+          </div>
+          <div>
+              <p>{this.state.characters}</p>
+          </div>
+          <input id="submit-btn" type="submit" value="Submit" />
+      </form>
     );
   }
 }
