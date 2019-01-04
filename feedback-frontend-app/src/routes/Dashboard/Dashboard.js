1763379468c6e7a5898dc070e18b16e648e02ca5
Songkeys <zy22132@nottingham.edu.cn>
2019-01-04 00:47:45 +0000

    Add feedback list to dashboard

--- feedback-frontend-app/src/routes/Dashboard/Dashboard.js
+++ feedback-frontend-app/src/routes/Dashboard/Dashboard.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Card, Spin, message } from "antd";
+import { Card, Spin, Comment, List, message, Rate } from "antd";
 import api from "../../utils/Api";
 
 const Dashboard = () => {
   const [isLoading, setIsLoading] = React.useState(false);
-  const [count, setCount] = React.useState(0);
+  const [feedbackList, setFeedbackList] = React.useState([]);
 
   React.useEffect(async () => {
     setIsLoading(true);
     try {
       const data = await api.request("feedback");
-      const count = data._embedded.feedbackList.length;
-      setCount(count);
+      setFeedbackList(data._embedded.feedbackList);
     } catch (e) {
       message.error(e.toString());
     } finally {
@@ -24,8 +23,25 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       <Spin tip="Loading..." spinning={isLoading} delay={500}>
         <Card title="VOLUME" style={{ width: 300 }}>
-          <h2>{count}</h2>
+          <h2>{feedbackList.length}</h2>
         </Card>
+        <List
+          className="comment-list"
+          header="Feedback"
+          itemLayout="horizontal"
+          dataSource={feedbackList}
+          renderItem={feedback => (
+            <Comment
+              content={feedback.text}
+              datetime={
+                <>
+                  {feedback.sentiment}
+                  <Rate disabled defaultValue={feedback.rating} />
+                </>
+              }
+            />
+          )}
+        />
       </Spin>
     </div>
   );
