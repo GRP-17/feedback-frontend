f6e97dce339b482152aec46385b3802098ef96ac
Songkeys <zy22132@nottingham.edu.cn>
2019-01-05 04:49:37 +0000

    fix https request

--- feedback-frontend-app/src/config.js
+++ feedback-frontend-app/src/config.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://feedback-analysis-grp-app.herokuapp.com/";
+export const baseUrl = "https://feedback-analysis-grp-app.herokuapp.com/";
 export const appUrl = "https://feedback-analysis.herokuapp.com/";
 // export const corsHelper = "https://cors-anywhere.herokuapp.com/";
--- feedback-frontend-app/src/utils/Api.js
+++ feedback-frontend-app/src/utils/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { baseUrl, appUrl /*corsHelper*/ } from "../config";
+import { baseUrl, appUrl /* , corsHelper */ } from "../config";
 
 /**
  * Usage:
