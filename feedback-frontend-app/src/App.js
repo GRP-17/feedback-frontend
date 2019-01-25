8277f60b5ef5299609cb880bb8350f4ca9161bdc
Songkeys <zy22132@nottingham.edu.cn>
2019-01-25 16:01:30 +0000

    Delete index and feedback routes

--- feedback-frontend-app/src/App.js
+++ feedback-frontend-app/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Index from "./routes/Index/Index";
-import Feedback from "./routes/Feedback/Feedback";
 import Dashboard from "./routes/Dashboard/Dashboard";
 import "./App.css";
 
@@ -9,9 +7,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={Index} />
-        <Route path="/feedback" component={Feedback} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route exact path="/" component={Dashboard} />
       </div>
     </Router>
   );
