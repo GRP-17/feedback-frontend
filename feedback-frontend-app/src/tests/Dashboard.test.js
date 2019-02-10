8a088ff9f257103c4590cff551e18942a067935b
George <psygh3@nottingham.ac.uk>
2019-02-10 17:49:11 +0000

    forgot to amend one of the tests

--- feedback-frontend-app/src/tests/Dashboard.test.js
+++ feedback-frontend-app/src/tests/Dashboard.test.js
@@ -121,17 +121,19 @@ describe("Dashboard", () => {
         await eWrap.instance().getData();
 
         //check it was called twice
-        expect(Api.request.mock.calls.length).toEqual(3);
+        expect(Api.request.mock.calls.length).toEqual(4);
 
         //check the first call was to feedback
         expect(Api.request.mock.calls[0][0]).toEqual("feedback");
 
         //check the second was to feedback_sentiment_count
-        expect(Api.request.mock.calls[1][0]).toEqual("feedback_sentiment_count");
-        
+        expect(Api.request.mock.calls[1][0]).toEqual(
+          "feedback_sentiment_count"
+        );
+
         //rating/count should be third call
         expect(Api.request.mock.calls[2][0]).toEqual("feedback_rating_count");
-        
+
         //rating/average should be fourth
         expect(Api.request.mock.calls[3][0]).toEqual("feedback_average_rating");
       });
