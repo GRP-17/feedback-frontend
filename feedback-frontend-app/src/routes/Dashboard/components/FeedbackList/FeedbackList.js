f50c7d9fdc8c6f7811fa8420167c18f6fdaaf8c1
Songkeys <zy22132@nottingham.edu.cn>
2019-02-11 16:00:33 +0000

    Complete feedback list. close #8

--- feedback-frontend-app/src/routes/Dashboard/components/FeedbackList/FeedbackList.js
+++ feedback-frontend-app/src/routes/Dashboard/components/FeedbackList/FeedbackList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Comment, List, Rate } from "antd";
+import { List, Rate } from "antd";
 import PropTypes from "prop-types";
 
 export default function FeedbackList(props) {
@@ -7,22 +7,38 @@ export default function FeedbackList(props) {
     dataSource: PropTypes.array.isRequired
   };
 
+  const renderItem = feedback => {
+    const sentiment2color = {
+      'POSITIVE': '#2b9588',
+      'NEUTRAL': '#eee',
+      'NEGATIVE': '#e44a5b',
+    }
+
+    return <List.Item>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span style={{
+          backgroundColor: sentiment2color[feedback.sentiment],
+          height: 10,
+          width: 10,
+          borderRadius: '50%',
+          marginRight: 5,
+        }} />
+        <Rate disabled defaultValue={feedback.rating} />
+      </div>
+      <div style={{
+        height: 100,
+        overflow: 'scroll',
+      }}>{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}{feedback.text}</div>
+    </List.Item>
+  }
+
   return (
     <List
       header="Feedback"
-      itemLayout="horizontal"
+      itemLayout="vertical"
       dataSource={props.dataSource}
-      renderItem={feedback => (
-        <Comment
-          content={feedback.text}
-          datetime={
-            <>
-              {feedback.sentiment}
-              <Rate disabled defaultValue={feedback.rating} />
-            </>
-          }
-        />
-      )}
+      locale={{ emptyText: 'No feedback' }}
+      renderItem={feedback => renderItem(feedback)}
     />
   );
 }
